18ab3cc638c13aca56edf9c59a07522a2fe65c0c
Restyled.io <commits@restyled.io>
2019-09-15 06:22:22 +0000

    Restyled by shfmt

--- nadi.sh
+++ nadi.sh
@@ -1,17 +1,16 @@
-cd program files 
-cd git 
+cd program files
+cd git
 git-bash
-
 
 #!/bin/bash# Choose which version of yarn you want to use
 EXPECTED_YARN_VERSION="0.16.1"
 
-function install_yarn {
+function install_yarn() {
   mkdir -p .yarn
   DOWNLOAD_URL="https://github.com/yarnpkg/yarn/releases/download/v$EXPECTED_YARN_VERSION/yarn-v$EXPECTED_YARN_VERSION.tar.gz"
   echo "Downloading from $DOWNLOAD_URL"
-  curl -fL "$DOWNLOAD_URL" > .yarn/yarn.tar.gz
-  tar zxf .yarn/yarn.tar.gz  --strip-components=1 -C .yarn
+  curl -fL "$DOWNLOAD_URL" >.yarn/yarn.tar.gz
+  tar zxf .yarn/yarn.tar.gz --strip-components=1 -C .yarn
 }
 
 if [ -f .yarn/bin/yarn ]; then
